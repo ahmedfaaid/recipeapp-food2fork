b20d4c0daf68145e001fedb4e8cf7363e317aa86
Ahmed <ahmedfaaid.coding@gmail.com>
2019-08-13 18:44:35 -0400

    Add folders and files, Recipe, RecipeDetails and fetch added

---
+++ src/components/RecipeDetails.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { recipe } from '../setup-files/tempDetails';
+
+class RecipeDetails extends Component {
+	render() {
+		return (
+			<>
+				<h4>Hello from details</h4>
+			</>
+		);
+	}
+}
+
+export default RecipeDetails;
---
+++ src/components/RecipeSearch.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RecipeSearch extends Component {
+	render() {
+		return (
+			<>
+				<h2>Hello from search</h2>
+			</>
+		);
+	}
+}
+
+export default RecipeSearch;
