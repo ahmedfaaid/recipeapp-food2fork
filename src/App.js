6c90c291982bf1ffc476249bbd9cb8b036762718
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2019-08-15 21:59:23 -0400

    Remove setup-files import from App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import { recipes } from './setup-files/tempList';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 
